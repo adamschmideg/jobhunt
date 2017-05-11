e317ac9c6b0e58eadc5d717dcc2abbc5422d7700
Adam Schmideg <adam@schmideg.net>
2017-05-11 13:45:15 +0200

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jobhunt
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/jobhunt/core_test.clj
@@ -0,0 +1,7 @@
+(ns jobhunt.core-test
+  (:require [clojure.test :refer :all]
+            [jobhunt.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
